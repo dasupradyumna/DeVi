b7703f733c37e085c391b88e468794a155189de6
Dasu Pradyumna <dasupradyumna@gmail.com>
2023-11-08 02:56:38 +0530

    feat(core): added `devi::core::shape` for array dimensionality
      This class represents the dimensionality of array-like objects and
      is responsible for all logic concerning their shape and size

--- test/core/array.cc
+++ test/core/array.cc
@@ -2,7 +2,8 @@
 
 int main()
 {
-  devi::core::bool8 t;
+  devi::core::bool8 t { devi::core::shape(3, 4) };
+
   devi::core::native_type<devi::core::type::float32>::type f { 3.145 };
   std::cout << f << std::endl;
 }
